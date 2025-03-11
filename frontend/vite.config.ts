1813cbaed1a135a48bb02a920c865c49800975c4
Javier Garrucho <jgarrucho1998@gmail.com>
2025-03-11 22:27:14 +0100

    fix: remove autoprefixer plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import autoprefixer from 'autoprefixer'
+// import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [vue(), vueDevTools(), tailwind(), autoprefixer()],
+    plugins: [vue(), vueDevTools(), tailwind()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
