74ea7340bacacab3bb393e157097b1a8cf700c84
Javier Garrucho <jgarrucho1998@gmail.com>
2025-03-24 22:41:02 +0100

    feat: add stop button

--- frontend/src/views/ScanView.vue
+++ frontend/src/views/ScanView.vue
@@ -5,6 +5,7 @@ import useRecord from '@/composables/use-record';
 import ShazamService from '@/services/shazam';
 import { audioChunksToBase64 } from '@/utils/audio-utils';
 import { Button } from '@/components/ui/button';
+import { X } from 'lucide-vue-next';
 
 import { useTrackDataStore } from '@/stores/track-data';
 
@@ -30,6 +31,11 @@ function onClickRecord() {
     stopRecording();
     startRecording();
   }, 4000);
+}
+
+function onClickStop() {
+  clearInterval(recordingInterval.value);
+  stopRecording();
 }
 
 const queryParams: {
@@ -62,12 +68,17 @@ onBeforeUnmount(() => {
 
 <template>
   <h1 class="text-center text-3xl">Escanea una cancion para comenzar</h1>
-  <Button
-    class="neon-button m-auto rounded-full bg-gradient-to-br from-slate-800 to-slate-900 text-xl text-white"
-    @click="onClickRecord"
-  >
-    {{ isRecording ? 'Escuchando...' : 'Escanear' }}
-  </Button>
+  <div class="m-auto flex flex-col justify-center space-y-12">
+    <Button
+      class="neon-button rounded-full bg-gradient-to-br from-slate-800 to-slate-900 text-xl text-white"
+      @click="onClickRecord"
+    >
+      {{ isRecording ? 'Escuchando...' : 'Escanear' }}
+    </Button>
+    <Button class="mx-auto w-9 rounded-full bg-slate-600 text-white" @click="onClickStop">
+      <X />
+    </Button>
+  </div>
 </template>
 <style lang="css" scoped>
 .neon-button {
